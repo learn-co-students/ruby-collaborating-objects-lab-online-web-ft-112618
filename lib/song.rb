76f92512c514810a33db871120b773c97530e105
adudley78 <dudleyadam@gmail.com>
2018-12-08 15:14:53 -0500

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,7 @@
+require 'pry'
+
 class Song
-  attr_accessor :name, :artist_name
+  attr_accessor :name
 
   @@all = [ ]
 
@@ -10,6 +12,26 @@ class Song
 
   def self.all
     @@all
+  end
+
+  def artist=(artist)
+    @artist = artist
+  end
+
+  def artist
+    @artist
+  end
+
+  def self.new_by_filename(file_name)
+    #binding.pry
+    song_name = file_name.split(" - ")[1]
+    artist_name = file_name.split(" - ")[0]
+    #binding.pry
+    artist = Artist.find_or_create_by_name(artist_name) #artist is instance of Artist model
+    song = Song.new(song_name) #associate song instance with artist from split filename above
+    song.artist = artist #artist is a property (instance method) of song
+    artist.add_song(song)
+    song
   end
 
   # def artist_name
