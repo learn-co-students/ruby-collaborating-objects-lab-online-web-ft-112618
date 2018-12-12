2e44ca829f45cf89b883019b3304eeedbd37aad0
Jonathan Harwood <jhharwood88@gmail.com>
2018-12-12 03:34:40 +0000

    Done.

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,21 @@
+class MP3Importer
+  
+  attr_accessor :path 
+  
+  def initialize(path)
+    @path=path
+  end
+  
+  def files
+    Dir.glob("#{path}/*.mp3").map do |file|
+      file.gsub("#{path}/" , "")
+    end
+    
+  end
+  
+  
+  def import
+    self.files.each {|file| Song.new_by_filename(file) }   
+  end
+  
+end
